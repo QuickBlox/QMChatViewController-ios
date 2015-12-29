4487c92f3344ea8723820fceacba28efaef9b4f3
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2015-12-29 14:44:39 +0200

    Added missing inline doc

--- QMChatViewController/DataSource/QMMessagesDataSource.h
+++ QMChatViewController/DataSource/QMMessagesDataSource.h
@@ -29,12 +29,49 @@
  */
 @property (nonatomic, assign) NSTimeInterval timeIntervalBetweenSections;
 
+/**
+ *  Adding messages to the top of the data source.
+ *
+ *  @param messages messages to add
+ *
+ *  @return dictionary with added sections indexes and added items index paths
+ */
 - (NSDictionary *)addMessagesToTop:(NSArray QB_GENERIC(QBChatMessage *) *)messages;
+
+/**
+ *  Adding messages to the bottom of the data source.
+ *
+ *  @param messages messages to add
+ *
+ *  @return dictionary with added sections indexes and added items index paths
+ */
 - (NSDictionary *)addMessagesToBottom:(NSArray QB_GENERIC(QBChatMessage *) *)messages;
 
+/**
+ *  Replace message in the data source.
+ *
+ *  @param message message replace
+ *
+ *  @return index path of replaced message
+ */
 - (NSIndexPath *)replaceMessage:(QBChatMessage *)message;
+
+/**
+ *  Replace messages in the data source.
+ *
+ *  @param messages messages to replace
+ *
+ *  @return array of index paths of replaced messages
+ */
 - (NSArray QB_GENERIC(NSIndexPath *) *)replaceMessages:(NSArray QB_GENERIC(QBChatMessage *) *)messages;
 
+/**
+ *  Remove messages from the data source.
+ *
+ *  @param messages messages to remove
+ *
+ *  @return dictionary of removed sections indexes and items index paths
+ */
 - (NSDictionary *)removeMessages:(NSArray QB_GENERIC(QBChatMessage *) *)messages;
 
 /**
