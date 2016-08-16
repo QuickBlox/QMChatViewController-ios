b617c86ea18d5e2c4a9dc39df6255a12e8b0e019
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2016-08-16 23:54:23 +0300

    added date divider

---
+++ QMChatViewController/QMChatDataSource/QBChatMessage+QBDateDivider.h
@@ -0,0 +1,13 @@
+//
+//  QBChatMessage+QBDateDivider.h
+//  sample-chat
+//
+//  Created by Vitaliy Gurkovsky on 8/16/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import <Quickblox/Quickblox.h>
+
+@interface QBChatMessage (QBDateDivider)
+
+@end
---
+++ QMChatViewController/QMChatDataSource/QBChatMessage+QBDateDivider.m
@@ -0,0 +1,13 @@
+//
+//  QBChatMessage+QBDateDivider.m
+//  sample-chat
+//
+//  Created by Vitaliy Gurkovsky on 8/16/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "QBChatMessage+QBDateDivider.h"
+
+@implementation QBChatMessage (QBDateDivider)
+
+@end
