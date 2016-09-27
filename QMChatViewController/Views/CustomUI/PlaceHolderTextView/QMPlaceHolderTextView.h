8d353bea70e5a38a35d4eafea09c1186c316b813
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2016-09-27 14:15:10 +0300

    minor changes

--- QMChatViewController/Views/CustomUI/PlaceHolderTextView/QMPlaceHolderTextView.h
+++ QMChatViewController/Views/CustomUI/PlaceHolderTextView/QMPlaceHolderTextView.h
@@ -72,18 +72,4 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-@protocol QMPlaceHolderTextViewPasteDelegate <NSObject>
-
-@optional
-/**
- *  Asks the delegate whether or not the `textView` should use the original implementation of `-[UITextView paste]`.
- *
- *  @discussion Use this delegate method to implement custom pasting behavior.
- *  You should return `NO` when you want to handle pasting.
- *  Return `YES` to defer functionality to the `textView`.
- */
-- (BOOL)placeHolderTextView:(QB_NONNULL QMPlaceHolderTextView *)textView shouldPasteWithSender:(QB_NONNULL id)sender;
-
-
-@end
 NS_ASSUME_NONNULL_END
