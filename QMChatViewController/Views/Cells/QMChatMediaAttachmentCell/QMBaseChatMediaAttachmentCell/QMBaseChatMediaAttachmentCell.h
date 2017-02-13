cd7a71353fda91919a3ca13e5dea496aa441dabf
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-02-13 19:05:46 +0200

    added QMBaseChatMediaAttachmentCell

---
+++ QMChatViewController/Views/Cells/QMChatMediaAttachmentCell/QMBaseChatMediaAttachmentCell/QMBaseChatMediaAttachmentCell.h
@@ -0,0 +1,15 @@
+//
+//  QMBaseChatMediaAttachmentCell.h
+//  Pods
+//
+//  Created by Vitaliy Gurkovsky on 2/07/17.
+//
+//
+
+#import "QMChatCell.h"
+
+@protocol QMMediaViewDelegate;
+
+@interface QMBaseChatMediaAttachmentCell : QMChatCell <QMMediaViewDelegate>
+
+@end
---
+++ QMChatViewController/Views/Cells/QMChatMediaAttachmentCell/QMBaseChatMediaAttachmentCell/QMBaseChatMediaAttachmentCell.m
@@ -0,0 +1,15 @@
+//
+//  QMBaseChatMediaAttachmentCell.m
+//  Pods
+//
+//  Created by Vitaliy Gurkovsky on 2/07/17.
+//
+//
+
+#import "QMBaseChatMediaAttachmentCell.h"
+#import "QMMediaViewDelegate.h"
+
+@implementation QMBaseChatMediaAttachmentCell
+@synthesize presenter;
+
+@end
