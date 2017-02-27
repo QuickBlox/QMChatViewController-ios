ef997103a3ee9725f375d5f8fb81b5d7239b99f1
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2017-02-27 14:08:03 +0200

    added image incoming and outgoing cell

---
+++ QMChatViewController/Views/Cells/QMChatMediaAttachmentCell/QMImageOutgoingCell/QMImageOutgoingCell.m
@@ -0,0 +1,15 @@
+//
+//  QMImageOutgoingCell.m
+//  Pods
+//
+//  Created by Vitaliy Gurkovsky on 2/27/17.
+//
+//
+
+#import "QMImageOutgoingCell.h"
+
+@implementation QMImageOutgoingCell
+
+
+
+@end
