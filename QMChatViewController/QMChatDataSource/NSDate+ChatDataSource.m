3baa77f360f7c10a7051cd773d2b0dd6c43f3aa4
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2017-06-15 15:34:39 +0300

    NSDate+ChatDataSource using QMDateUtils instead of own custom string implementation

--- QMChatViewController/QMChatDataSource/NSDate+ChatDataSource.m
+++ QMChatViewController/QMChatDataSource/NSDate+ChatDataSource.m
@@ -7,8 +7,7 @@
 //
 
 #import "NSDate+ChatDataSource.h"
-
-const NSCalendarUnit componentFlags = (NSCalendarUnitYear| NSCalendarUnitMonth | NSCalendarUnitDay |  NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond);
+#import "QMDateUtils.h"
 
 @implementation NSDate (ChatDataSource)
 
@@ -50,7 +49,6 @@ const NSCalendarUnit componentFlags = (NSCalendarUnitYear| NSCalendarUnitMonth |
     dateComponents.day = 1;
     dateComponents.second = -1;
     
- 
     NSDate *endDate =  [[self calendar] dateByAddingComponents:dateComponents
                                                   toDate:[self dateAtStartOfDay]
                                                  options:0];
@@ -59,124 +57,12 @@ const NSCalendarUnit componentFlags = (NSCalendarUnitYear| NSCalendarUnitMonth |
 
 - (NSString *)stringDate {
     
-    return [self formattedStringFromDate];
+    return [QMDateUtils formattedStringFromDate:self];
 }
 
 - (BOOL)isBetweenStartDate:(NSDate *)startDate andEndDate:(NSDate *)endDate {
     return ([self compare:startDate] == NSOrderedDescending &&
             [self compare:endDate]  == NSOrderedAscending);
-}
-
-- (NSString *)stringDateWithFormat:(NSString *)dateFormat {
-    
-    static NSDateFormatter *dateFormatter;
-    static dispatch_once_t onceToken;
-    
-    dispatch_once(&onceToken, ^{
-        dateFormatter = [[NSDateFormatter alloc] init];
-        dateFormatter.locale = [NSLocale currentLocale];
-        dateFormatter.timeZone = [self calendar].timeZone;
-        [dateFormatter setDateFormat:@"d MMMM YYYY"];
-    });
-    
-    if (dateFormat.length) {
-        [dateFormatter setDateFormat:dateFormat];
-    }
-    
-    return [dateFormatter stringFromDate:self];
-}
-
-- (NSString *)formattedStringFromDate
-{
-    NSString *formattedString = nil;
-    NSDateComponents *components = [[self calendar] components:NSCalendarUnitDay | NSCalendarUnitWeekOfMonth | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:self];
-    NSDateComponents *currentComponents = [[self calendar] components:NSCalendarUnitDay | NSCalendarUnitWeekOfMonth | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
-    
-    if (components.day == currentComponents.day && components.month == currentComponents.month && components.year == currentComponents.year) {
-        
-        formattedString = [self formatDateForDayRange];
-    }
-    else if (components.day == currentComponents.day - 1 && components.month == currentComponents.month && components.year == currentComponents.year) {
-        
-        formattedString = [self formatDateForDayRange];
-    }
-    else if (components.weekOfMonth == currentComponents.weekOfMonth && components.month == currentComponents.month && components.year == currentComponents.year) {
-        
-        formattedString = [self formatDateForWeekRange];
-    }
-    else if (components.year == currentComponents.year) {
-        
-        formattedString = [self formatDateForMonthRange];
-    }
-    else {
-        
-        formattedString = [self formatDateForYearRange];
-    }
-    
-    return formattedString;
-}
-
-- (NSString *)formatDateForTimeRange:(NSDate *)date
-{
-    static NSDateFormatter* formatter;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        formatter = [NSDateFormatter new];
-        formatter.dateStyle = NSDateFormatterNoStyle;
-        formatter.timeStyle = NSDateFormatterShortStyle;
-    });
-    
-    return [formatter stringFromDate:date];
-}
-
-- (NSString *)formatDateForDayRange
-{
-    static NSDateFormatter* formatter;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        formatter = [NSDateFormatter new];
-        formatter.dateStyle = NSDateFormatterMediumStyle;
-        formatter.timeStyle = NSDateFormatterNoStyle;
-        formatter.doesRelativeDateFormatting = YES;
-    });
-    
-    return [formatter stringFromDate:self];
-}
-
-- (NSString *)formatDateForWeekRange
-{
-    static NSDateFormatter* formatter;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        formatter = [NSDateFormatter new];
-        formatter.dateFormat = @"EEEE";
-    });
-    
-    return [formatter stringFromDate:self];
-}
-
-- (NSString *)formatDateForMonthRange
-{
-    static NSDateFormatter* formatter;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        formatter = [NSDateFormatter new];
-        formatter.dateFormat = @"LLLL d";
-    });
-    
-    return [formatter stringFromDate:self];
-}
-
-- (NSString *)formatDateForYearRange
-{
-    static NSDateFormatter* formatter;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        formatter = [NSDateFormatter new];
-        formatter.dateFormat = @"LLLL d y";
-    });
-    
-    return [formatter stringFromDate:self];
 }
 
 @end
